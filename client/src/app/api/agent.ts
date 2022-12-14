12a4f4aa6a87b2b816d31d1a55eedfbeefa54beb
Venkat Avula <nagarjunareddy04@gmail.com>
2022-12-14 10:55:34 +0000

    End Of Section 8

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
+import { PaginatedResponse } from "../models/pagination";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -14,6 +15,11 @@ const responseBody = (response: AxiosResponse) => response.data;
 
 axios.interceptors.response.use(async response => {
     await sleep(1000);
+    const pagination = response.headers["pagination"];
+    if(pagination){
+        response.data = new PaginatedResponse(response.data, JSON.parse(pagination));
+        return response;
+    }
     return response;
 }, (error: AxiosError) => {
     console.log("caught by interceptor");
@@ -36,16 +42,17 @@ axios.interceptors.response.use(async response => {
 })
 
 const requests = {
-    get: (url: string) => axios.get(url).then(responseBody),
+    get: (url: string, params?: URLSearchParams) => axios.get(url, {params}).then(responseBody),
     post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
     put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
     delete: (url: string) => axios.delete(url).then(responseBody),
 }
 
 const Catalog = {
-    list: () => requests.get("/products"),
-    details: (id?: string) => requests.get(`/products/${id}`)
-}
+    list: (params: URLSearchParams) => requests.get("/products", params),
+    details: (id?: string) => requests.get(`/products/${id}`),
+    fetchFilters: () => requests.get('/products/filters')
+}   
 
 const TestErrors = {
     get400Error: () => requests.get("/buggy/bad-request"),
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,15 +1,27 @@
+import { Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, Pagination, Paper, Radio, RadioGroup, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import { Fragment, useEffect, useState } from "react";
 import agent from "../../app/api/agent";
+import AppPagination from "../../app/components/AppPagination";
+import CheckboxButtons from "../../app/components/CheckboxButtons";
+import RadioButtonGroup from "../../app/components/RadioButtonGroup";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { fetchFilters, fetchProductsAsync, productSelectors, setPageNumber, setProductParams } from "./catalogSlice";
 import ProductList from "./ProductList";
+import ProductSearch from "./ProductSearch";
+
+const sortOptions = [
+    { value: "name", label: "Alphabetical" },
+    { value: "priceDesc", label: "Price - High to Low" },
+    { value: "price", label: "Price - Low to High" }
+];
 
 export default function Catalog() {
     ////const [products, setProducts] = useState<Product[]>([]);
     const products = useAppSelector(productSelectors.selectAll);
-    const {productLoaded, status} = useAppSelector(state => state.catalog);
+    const { productLoaded, status, filtersLoaded, brands, types, productParams, metaData } = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
     //const [loading, setLoading] = useState(true);
 
@@ -22,16 +34,50 @@ export default function Catalog() {
     // }, [])
 
     useEffect(() => {
-        if(!productLoaded){
-            dispatch(fetchProductsAsync());  
-        } 
-    }, [productLoaded])
+        if (!productLoaded) {
+            dispatch(fetchProductsAsync());
+        }
+    }, [productLoaded, dispatch])
 
-    if(status.includes('pending')) return <LoadingComponent message="Loading Products..." />
+    useEffect(() => {
+        if (!filtersLoaded) {
+            dispatch(fetchFilters());
+        }
+    }, [dispatch, filtersLoaded])
+
+    if (!filtersLoaded) return <LoadingComponent message="Loading Products..." />
 
     return (
         <>
-            <ProductList products={products} />
+            <Grid container columnSpacing={4}>
+                <Grid item xs={3}>
+                    <Paper sx={{ mb: 2 }}>
+                        <ProductSearch />
+                    </Paper>
+                    <Paper sx={{ mb: 2, p: 2 }}>
+                        <RadioButtonGroup
+                            selectedValue={productParams.orderBy}
+                            options={sortOptions}
+                            onChange={(e) => dispatch(setProductParams({ orderBy: e.target.value }))} />
+                    </Paper>
+                    <Paper sx={{ mb: 2, p: 2 }}>
+                        <CheckboxButtons items={brands} checked={productParams.brands}
+                            onChange={(items: string[]) => dispatch(setProductParams({ brands: items }))} />
+                    </Paper>
+                    <Paper sx={{ mb: 2, p: 2 }}>
+                        <CheckboxButtons items={types} checked={productParams.types}
+                            onChange={(items: string[]) => dispatch(setProductParams({ types: items }))} />
+                    </Paper>
+                </Grid>
+                <Grid item xs={9}>
+                    <ProductList products={products} />
+                </Grid>
+                <Grid item xs={3} />
+                <Grid item xs={9} sx={{ mb: 2 }} >
+                    {metaData &&
+                        <AppPagination metaData={metaData} onPageChange={(page: number) => dispatch(setPageNumber({ pageNumber: page }))} />}
+                </Grid>
+            </Grid>
         </>
     )
 }
